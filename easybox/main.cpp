3e54d7f2deb1e2258791cfa61036213d95be579a
SimpleJack <aykut@Aykuts-iMac.local>
2013-09-29 22:05:48 +0200

    Initial Commit

---
+++ easybox/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  easybox
+//
+//  Created by Aykut René Inan on 29.09.13.
+//  Copyright (c) 2013 Aykut René Inan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
